9a94bc5c6fa518831d075253a3306a8a8027bea2
crandonriordan <crandonriordan@gmail.com>
2020-11-23 20:41:07 -0600

    using a marked parser and sanitizing the html

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,44 +1,10 @@
 import React from "react";
+import marked from "marked";
+import sanitizeHtml from "sanitize-html";
 
 const parseText = (text) => {
     if(text) {
-        const lines = text.split("\n");
-        const output = lines.map((line) => {
-            return parseLine(line);
-        });
-
-        console.log(output);
-        return output;
-    } else {
-        return null;
-    }
-}
-
-const parseLine = (line) => {
-    if(line.startsWith("######")) {
-        return (
-            <h6>{line.substring(6)}</h6>
-        )
-    } else if (line.startsWith("#####")) {
-        return (
-            <h5>{line.substring(5)}</h5>
-        )
-    } else if (line.startsWith("####")) {
-        return (
-            <h4>{line.substring(4)}</h4>
-        )
-    } else if (line.startsWith("###")) {
-        return (
-            <h3>{line.substring(3)}</h3>
-        )
-    } else if (line.startsWith("##")) {
-        return (
-            <h2>{line.substring(2)}</h2>
-        )
-    } else if (line.startsWith("#")) {
-        return (
-            <h1>{line.substring(1)}</h1>
-        )
+        return sanitizeHtml(marked(text));
     } else {
         return null;
     }
@@ -49,7 +15,8 @@ const Previewer = (props) => {
         <div className="flex-child">
             <h1 className="preview-header">Preview</h1>
             <hr/>
-            { parseText(props.text) }
+            <div dangerouslySetInnerHTML={{__html: parseText(props.text)}} >
+            </div>
         </div>
     )
 }
